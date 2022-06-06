32adee99f3b6d908850e6b22981592af7bb59a7f
Maxiversum <git@maxiversum.com>
2022-06-06 17:26:36 +0200

    Add index page

---
+++ app.js
@@ -0,0 +1,8 @@
+
+/**
+ * The loader will disappear after the document is fully loaded.
+ */
+ window.addEventListener("load", () => {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("loader-div").style.display = "none";
+})
